2dd937ec30d88dd43c1f8f231c278b95b12408ff
Daniel Juárez <danieljuarezp@hotmail.com>
2023-05-19 18:30:23 -0600

    init changes

--- src/app/temperatureConverter/temperatureConverter.component.ts
+++ src/app/temperatureConverter/temperatureConverter.component.ts
@@ -7,10 +7,22 @@ import {Component, Input, OnInit} from '@angular/core';
 })
 
 export class TemperatureConverter implements OnInit {
+  celsius = null;
+  fahrenheit = null;
 
   ngOnInit() {
     // C = (F − 32) × 5/9
     // F = C*9/5 + 32
+  }
+
+  updateCelsius(value: number) {
+    this.celsius = value;
+    this.fahrenheit = value *9/5 + 32;
+  }
+
+  updateFahrenheit(value: number) {
+    this.fahrenheit = value;
+    this.celsius = (value - 32) * 5/9;
   }
 
 }
